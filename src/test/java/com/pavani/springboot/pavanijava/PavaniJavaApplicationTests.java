1660059729461479699783ac6e1fe3b6b389dcb4
Pavani Maruboina <pavanirajeswari.m@gmail.com>
2020-07-09 18:51:47 -0400

    sample springboot

---
+++ src/test/java/com/pavani/springboot/pavanijava/PavaniJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavani.springboot.pavanijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavaniJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
