2c4850cdea097cedcc40635af482e482f364504b
Pavani Maruboina <pavanirajeswari.m@gmail.com>
2020-07-10 12:12:49 -0400

    Adding rest end points

---
+++ src/test/java/com/pavani/springboot/pavanijava/PavaniJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pavani.springboot.pavanijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavaniJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PavaniJavaApplication.class, args);
+    }
+
+}
